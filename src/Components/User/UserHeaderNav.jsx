6d4e6500a03b9512111a4f924b7276cf756626fe
Lucas Eduardo <luke_1999queiroz@hotmail.com>
2023-08-17 13:50:09 -0300

    implementações mobile e criação de navegação de usuario

---
+++ src/Components/User/UserHeaderNav.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { userContext } from '../../userContext';
+import { ReactComponent as MinhasFotos } from '../../Assets/feed.svg';
+import { ReactComponent as AdicionarFoto } from '../../Assets/adicionar.svg';
+import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
+import { ReactComponent as Sair } from '../../Assets/sair.svg';
+import styles from './UserHeaderNav.module.css';
+import { useNavigate } from 'react-router-dom';
+import useMedia from '../../Hooks/useMedia';
+
+const UserHeaderNav = () => {
+  const mobile = useMedia('(max-width: 40rem)');
+  const { userLogout } = React.useContext(userContext);
+  const navigate = useNavigate();
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
+  function handleLogout() {
+    userLogout();
+    navigate('/login');
+  }
+
+  return (
+    <>
+      {mobile && (
+        <button
+          aria-label="Menu"
+          className={`${styles.mobileButton} ${
+            mobileMenu && styles.mobileButtonActive
+          }`}
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></button>
+      )}
+
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${
+          mobileMenu && styles.navMobileActive
+        }`}
+      >
+        <NavLink to="/conta" end>
+          <MinhasFotos />
+          {mobile && 'Minhas Fotos'}
+        </NavLink>
+        <NavLink to="/conta/estatisticas">
+          <Estatisticas />
+          {mobile && 'Estatisticas'}
+        </NavLink>
+        <NavLink to="/conta/postar">
+          <AdicionarFoto />
+          {mobile && 'Adicionar Foto'}
+        </NavLink>
+        <button onClick={handleLogout}>
+          <Sair />
+          {mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
+  );
+};
+
+export default UserHeaderNav;
